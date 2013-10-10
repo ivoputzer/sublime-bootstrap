5ca4198943792a6f932aa70e8a881596c289ea5e
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-10-10 17:18:13 +0200

    fixing bug on copy failure and improving status messages

--- gather.sh
+++ gather.sh
@@ -2,11 +2,11 @@
 
 echo ">>> copying current sublime configurations to repository :"
 
-cp -r "/Applications/Sublime Text.app" "./Sublime Text.app" || echo "\033[1;31m ✗ You need to have Sublime Text installed to copy stuff! \033[0m"
+cp -r "/Applications/Sublime Text.app" . || echo "\033[1;31m ✗ You need to have Sublime Text installed to copy app! \033[0m"
 
-echo "\033[1;32m ✓ Sublime Text.app copied...\33[0m"
+echo "\033[1;32m ✓ Sublime Text.app copied...\033[0m"
 
-cp -r "~/Library/Application Support/Sublime Text 3/" "./Sublime Text 3" || echo "\033[1;31m ✗ You need to have Sublime Text installed to copy stuff! \033[0m"
+cp -r "$HOME/Library/Application Support/Sublime Text 3" . || echo "\033[1;31m ✗ You need to have Sublime Text installed to copy configs! \033[0m"
 
 echo "\033[1;32m ✓ Sublime configuration folder copied...\033[0m"
 
