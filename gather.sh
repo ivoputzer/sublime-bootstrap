ff822bdd70a773f5c67740d68a482bf808b4d1c2
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-10-10 21:14:22 +0200

    adjusting gather file to new specs

--- gather.sh
+++ gather.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-echo ">>> copying current sublime configurations to repository :"
+echo ">>> Copying application and configuration to repository"
 
-cp -r "/Applications/Sublime Text.app" "./Sublime Text.app" || echo "\033[1;31m ✗ You need to have Sublime Text installed to copy stuff! \033[0m"
+# removing current files
 
-echo "\033[1;32m ✓ Sublime Text.app copied...\33[0m"
+rm -rf "Sublime Text 3" "Sublime Text.app" || echo "\033[1;31m ✗ unable to remove files in this repository! \033[0m"
 
-cp -r "~/Library/Application Support/Sublime Text 3/" "./Sublime Text 3" || echo "\033[1;31m ✗ You need to have Sublime Text installed to copy stuff! \033[0m"
+echo "\033[1;32m ✓ Removing current files...\033[0m"
+
+# copying application
+
+cp -r "/Applications/Sublime Text.app" "Sublime Text.app/" &> /dev/null || echo "\033[1;31m ✗ You need to have Sublime Text installed to copy stuff! \033[0m"
+
+echo "\033[1;32m ✓ Sublime Text.app copied...\033[0m"
+
+# copying Configuration
+
+cp -r "$HOME/Library/Application Support/Sublime Text 3/" "Sublime Text 3/" &> /dev/null || echo "\033[1;31m ✗ You need to have Sublime Text installed to copy stuff! \033[0m"
 
 echo "\033[1;32m ✓ Sublime configuration folder copied...\033[0m"
 
-echo "\033[1;32m= done!\033[0m"
+# we're done, dude ;)
+
+echo "=== Done!"
